def66db696760d2597c6dbd94f54be9aa5e433b6
Andrzej Gołaszewski <golaszewski.a@gmail.com>
2019-11-13 17:03:54 +0100

    init

---
+++ src/Seq.App.Mailgun/MailgunReactor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentEmail.Core;
+using FluentEmail.Mailgun;
+using Seq.Apps;
+using Seq.Apps.LogEvents;
+
+namespace Seq.App.Mailgun
+{
+    [SeqApp("Mailgun",
+    Description = "Uses a mailgun to send events as email.")]
+    public class MailgunReactor : SeqApp, ISubscribeTo<LogEventData>
+    {
+        [SeqAppSetting(DisplayName = "Domain")]
+        public string Domain { get; set; }
+
+        [SeqAppSetting(DisplayName = "Api Key")]
+        public string ApiKey { get; set; }
+
+        [SeqAppSetting(DisplayName = "From Email Display Name")]
+        public string FromEmailName { get; set; }
+
+        [SeqAppSetting(DisplayName = "From Email")]
+        public string FromEmail { get; set; }
+
+        [SeqAppSetting(DisplayName = "To Email")]
+        public string ToEmail { get; set; }
+
+        [SeqAppSetting(DisplayName = "Subject")]
+        public string Subject { get; set; }
+
+        public void On(Event<LogEventData> evt)
+        {
+            SendEmail("Test");
+        }
+
+        public void SendEmail(string body)
+        {
+            var sender = new MailgunSender(Domain, ApiKey);
+            var email = Email.From(FromEmail).To(ToEmail).Subject(Subject).Body(body);
+            var response = email.Send();
+
+            if(!response.Successful)
+            {
+                throw new Exception(string.Join(" ; ", response.ErrorMessages));
+            }
+        }
+    }
+}
