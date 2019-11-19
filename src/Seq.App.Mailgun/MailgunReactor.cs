c32deb60fb228a83b7f5b0151c7e62f344f05ca2
Andrzej Gołaszewski <golaszewski.a@gmail.com>
2019-11-19 13:32:46 +0100

    -allow multiple emails addresses -add body to email

--- src/Seq.App.Mailgun/MailgunReactor.cs
+++ src/Seq.App.Mailgun/MailgunReactor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using FluentEmail.Core;
+using FluentEmail.Core.Models;
 using FluentEmail.Mailgun;
 using Seq.Apps;
 using Seq.Apps.LogEvents;
@@ -19,21 +21,27 @@ namespace Seq.App.Mailgun
         [SeqAppSetting(DisplayName = "From Email")]
         public string FromEmail { get; set; }
 
-        [SeqAppSetting(DisplayName = "To Email")]
+        [SeqAppSetting(DisplayName = "To Emails", InputType = SettingInputType.LongText)]
         public string ToEmail { get; set; }
 
         [SeqAppSetting(DisplayName = "Subject")]
         public string Subject { get; set; }
+
+        [SeqAppSetting(DisplayName = "Body", InputType = SettingInputType.LongText, IsOptional = true)]
+        public string Body { get; set; }
 
         public void On(Event<LogEventData> evt)
         {
             SendEmail(evt.Data.RenderedMessage);
         }
 
-        public void SendEmail(string body)
+        public void SendEmail(string renderedMessage)
         {
+            var recipients = ToEmail.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(emailAddress => new Address(emailAddress)).ToList();
+
             var sender = new MailgunSender(Domain, ApiKey);
-            var email = Email.From(FromEmail).To(ToEmail).Subject(Subject).Body(body);
+            var email = Email.From(FromEmail).To(recipients).Subject(Subject).Body(renderedMessage + Environment.NewLine + Body);
             var response = sender.Send(email);
 
             if(!response.Successful)
